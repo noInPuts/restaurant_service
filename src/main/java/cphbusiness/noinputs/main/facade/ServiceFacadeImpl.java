9d6bb64ae4630accfcdaf96634eaf26ca4997bd3
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-12-09 15:28:44 +0100

    Added ServiceFacade

---
+++ src/main/java/cphbusiness/noinputs/main/facade/ServiceFacadeImpl.java
@@ -0,0 +1,30 @@
+package cphbusiness.noinputs.main.facade;
+
+import cphbusiness.noinputs.main.dto.RestaurantDTO;
+import cphbusiness.noinputs.main.exception.RestaurantNotFoundException;
+import cphbusiness.noinputs.main.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceFacadeImpl implements ServiceFacade {
+
+    private final RestaurantService restaurantService;
+
+    @Autowired
+    public ServiceFacadeImpl(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @Override
+    public List<RestaurantDTO> getAllRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    @Override
+    public RestaurantDTO getRestaurant(Long id) throws RestaurantNotFoundException {
+        return restaurantService.getRestaurant(id);
+    }
+}
